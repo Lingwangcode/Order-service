c2a4443ef62a3135242266d28a571df9b4e122ed
Ling Wang <amyling.wang@gmail.com>
2023-05-29 11:18:06 +0200

    changed annotation in customer for address

--- src/main/java/com/example/orderservice/Models/Customer.java
+++ src/main/java/com/example/orderservice/Models/Customer.java
@@ -3,6 +3,7 @@ package com.example.orderservice.Models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,7 @@ public class Customer {
     @Size(min = 9, max = 13, message = "Ssn must be unique and between 9 and 13 characters")
     private String ssn;
     @Embedded
-    @NotBlank(message = "Must not be blank")
+    @NotNull(message = "Can not be empty")
     private Address address;
     @Email(message = "E-mail must be set properly")
     private String email;
