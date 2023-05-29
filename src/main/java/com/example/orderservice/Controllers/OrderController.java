0bcb2b3934f38b2a01142f483ad23a3cb75875e9
Maria Hagelin <mthagelin@gmail.com>
2023-05-29 08:32:00 +0200

    Få felmeddelanden också

--- src/main/java/com/example/orderservice/Controllers/OrderController.java
+++ src/main/java/com/example/orderservice/Controllers/OrderController.java
@@ -7,7 +7,10 @@ import com.example.orderservice.Repos.OrderRepo;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -75,5 +78,17 @@ public class OrderController {
     public @ResponseBody Customer[] getCustomers() {
 
         return restTemplate.getForObject(customerServiceUrl + "/customers/getAll" , Customer[].class);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
