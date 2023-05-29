6821904a6ab6b6e68a02f763a2ffdff83fda1fc6
Ling Wang <amyling.wang@gmail.com>
2023-05-29 10:30:46 +0200

    .

--- src/main/java/com/example/orderservice/RestTemplateConfig.java
+++ src/main/java/com/example/orderservice/RestTemplateConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
-
     private static final int CONNECTION_TIMEOUT = 5000;
     private static final int READ_TIMEOUT = 5000;
 
